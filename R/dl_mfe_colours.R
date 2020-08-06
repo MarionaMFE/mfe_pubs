81b68fe872e348ece81cba03d29db02eaa4ee560
MarionaMFE <mariona.roigevaliente@mfe.govt.nz>
2020-08-07 10:48:35 +1200

    change in color functions structure

--- R/dl_mfe_colours.R
+++
@@ -1,2 +0,0 @@
-mfe_colours <- c("#1C556C", "#32809C", "#0F7B7D", "#2C9986", "#6FC7B7", "#8B391D", "#DA5A28")
-image(volcano, col = mfe_colours)
---
+++ R/scale_fill_mfe_d.R
@@ -0,0 +1,41 @@
+#' MfE discrete filling for ggplot2 plots
+#'
+#' Function to apply MfE branded colours
+#' @param primary defaults to dark_green
+#' @param other defaults to orange
+#' @param direction defaults to 1
+#' @keywords ggplot-colour-formatting
+#' @export
+#' @examples
+#' scale_fill_mfe()
+
+# Isaac Bain: Easier way to use MfE colours in ggplot2
+# Inspiration from: https://www.garrickadenbuie.com/blog/custom-discrete-color-scales-for-ggplot2/
+# Mariona Roige: Have changed branded_pal function to add some user functionality to it
+# a- direction change from the other side of the palette
+# to use when looking for high contrasting colours. The blue side of the palette
+# is not distinguishible enough to the eye.
+
+# setup ggplot colour scale
+scale_fill_mfe <- function(
+  primary = "dark_blue",
+  other = "orange",
+  direction = 1,
+  ...
+) {
+  ggplot2::discrete_scale(
+    "fill", "mfe",
+    mfe_pal(primary, other, direction),
+    ...
+  )
+}
+
+# by convention colour also spelled without U.
+scale_fill_mfe <- scale_fill_mfe
+
+
+#mfe_pal()(2)
+#mfe_pal(other = 'dark_green')(2)
+#mfe_pal()(3)
+#mfe_pal(direction = -1)(3)
+
