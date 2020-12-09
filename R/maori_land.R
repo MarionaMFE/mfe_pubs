e4f03aaa4e27c325bd27ffb7464fc3957efa12a8
MarionaMFE <mariona.roigevaliente@mfe.govt.nz>
2020-12-09 18:15:21 +1300

    fixed maori land

---
+++ R/maori_land.R
@@ -0,0 +1,17 @@
+#' maori land dataset
+#'
+#' A brief description
+#'
+#' @docType data
+#'
+#' @usage data(maori_land)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(grav)
+#' times <- attr(grav, "time")
+#' phe <- grav$pheno
+#' \donttest{iplotCurves(phe, times)}
+
+"maori_land"
